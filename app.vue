d8b2b98aa9c1556f5b4566b1996664c54a6289cb
DevTeam <info@youshallthrive.com>
2024-09-08 12:49:08 -0400

    Initial commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup>
+// This file is the root of your app, and Nuxt will automatically inject the correct page components here
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  // No additional script needed for static content
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
